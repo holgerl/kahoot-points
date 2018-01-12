47e7249e6fdf3670d700c3f8a0c00970ac293474
Holger Ludvigsen <ludvigsen@gmail.com>
2018-01-12 23:08:59 +0100

    Implemented scores

--- components/Right.jsx
+++ components/Right.jsx
@@ -1,16 +1,55 @@
 import React from 'react';
+import {calculateScore, calculateScoreWithoutBonus} from '../service/scoreService.js'
+
+const itemRows = (items) => {
+  const keys = Object.keys(items);
+
+  keys.sort();
+  
+  return keys.map((key) => {
+    return (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{items[key]}</td>
+        <td>{calculateScore(key, items[key])}</td>
+      </tr>
+    );
+  });
+};
 
 const Right = ({items, resetItems}) => {
+
+  const sum = (a, b) => a + b;
+
+  const total = Object.entries(items)
+    .map(([key, value]) => calculateScore(key, value))
+    .reduce(sum, 0);
+
+  const totalWithoutBonuses = Object.entries(items)
+    .map(([key, value]) => calculateScoreWithoutBonus(key, value))
+    .reduce(sum, 0);
+
   return (
     <div className='right'>
       <div id="items">
         <h1>Player items</h1>
-        <p>A: {items.A}</p>
-        <p>B: {items.B}</p>
-        <p>C: {items.C}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Qty</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemRows(items)}
+          </tbody>
+        </table>
       </div>
+      
       <div id="totalScore">
-        <h2>Total score</h2>
+        <h2>Bonuses {total - totalWithoutBonuses}</h2>
+        <h2>Total {total}</h2>
         <a className="button control" href="#" onClick={resetItems}>New game</a>
         <p><a className="button control" href="#/admin">Admin</a></p>
       </div>
---
+++ service/scoreService.js
@@ -0,0 +1,31 @@
+
+const points = {
+  A: 50,
+  B: 30,
+  C: 20,
+  D: 15,
+}
+
+const bonus = {
+  A: [200, 3],
+  B: [90, 2],
+}
+
+export const calculateScore = (item, amount) => {
+  if (bonus[item] === undefined) {
+    return points[item] * amount;
+  } else {
+    const multiples = bonus[item][1];
+    const bonusScore = bonus[item][0];
+
+    const bonuses = Math.floor(amount/multiples);
+    
+    const rest = amount % multiples;
+
+    return bonusScore * bonuses + rest * points[item];
+  }
+};
+
+export const calculateScoreWithoutBonus = (item, amount) => {
+  return points[item] * amount;
+}
